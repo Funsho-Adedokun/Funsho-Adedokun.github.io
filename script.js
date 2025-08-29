5f76964548f09560ac34081a63147e9c1e72527a
Funsho-Adedokun <Adedokun.olufunso@gmail.com>
2025-08-29 06:57:08 +0100

    Portfolio site

---
+++ script.js
@@ -0,0 +1,11 @@
+function filterProjects(category) {
+    let projects = document.querySelectorAll(".project-card");
+
+    projects.forEach(project => {
+        if (category === "all" || project.dataset.category === category) {
+            project.style.display = "block";
+        } else {
+            project.style.display = "none";
+        }
+    });
+}
